4449275022c443d7432db1bece8a89153e0e8ec7
ParkSangMin <tkdals5402@mutant.ai.kr>
2026-08-10 20:58:48 +0900

    Release 7.0.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,11 @@ All notable changes to this package will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/)
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [7.0.4] - 2026-08-10
+
+### Changed
+- Renamed `RebuildLayoutByRectTransformTwice` to `RebuildLayoutBottomUp` and rebuild `ContentSizeFitter` layouts from the deepest hierarchy level upward.
+
 ## [7.0.3] - 2026-08-10
 
 ### Changed
--- Runtime/Scripts/Interfaces/ILayoutRebuildable.cs
+++ Runtime/Scripts/Interfaces/ILayoutRebuildable.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -15,11 +16,12 @@ namespace ParkMinPackages.Workflow.Default.Interfaces
 
 	public static class RectTransformLayoutRebuildableExtensions
 	{
-		public static void RebuildLayoutByRectTransformTwice(
+		public static void RebuildLayoutBottomUp(
 			this IRectTransformLayoutRebuildable layoutRebuildable
 		) {
 			ContentSizeFitter[] contentSizeFitters =
 				layoutRebuildable.LayoutRebuildTarget.GetComponentsInChildren<ContentSizeFitter>();
+			Array.Sort(contentSizeFitters, CompareByHierarchyDepthDescending);
 
 			foreach (ContentSizeFitter contentSizeFitter in contentSizeFitters) {
 				LayoutRebuilder.ForceRebuildLayoutImmediate(
@@ -30,6 +32,24 @@ namespace ParkMinPackages.Workflow.Default.Interfaces
 			LayoutRebuilder.ForceRebuildLayoutImmediate(
 				layoutRebuildable.LayoutRebuildTarget
 			);
+		}
+
+		static int CompareByHierarchyDepthDescending(
+			ContentSizeFitter left,
+			ContentSizeFitter right
+		) {
+			return GetHierarchyDepth(right.transform).CompareTo(GetHierarchyDepth(left.transform));
+		}
+
+		static int GetHierarchyDepth(Transform transform) {
+			int depth = 0;
+
+			while (transform != null) {
+				depth++;
+				transform = transform.parent;
+			}
+
+			return depth;
 		}
 	}
 }
